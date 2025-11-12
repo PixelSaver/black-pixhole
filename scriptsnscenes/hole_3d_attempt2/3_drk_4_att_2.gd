712abde602558fda14260de488f3d1289601fb7a
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-11-11 20:35:17 -0800

    Shader works, with a line of black dots that doesn't. Right click rotates camera around black hole

--- scriptsnscenes/hole_3d_attempt2/3_drk_4_att_2.gd
+++ scriptsnscenes/hole_3d_attempt2/3_drk_4_att_2.gd
@@ -1,1 +1,40 @@
 extends Control
+class_name RK4Attempt2
+
+@onready var shader_mat := $SubViewportContainer/SubViewport/ShaderRect.material as ShaderMaterial
+@export var pause : Control 
+var target := Vector3.ZERO
+var distance := 100
+var yaw := 0.0
+var pitch := 0.0
+var rotation_speed := 0.01
+var fov := 60.0
+
+func _ready():
+	pause.process_mode = Node.PROCESS_MODE_PAUSABLE
+	_update_shader_camera()
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		yaw -= event.relative.x * rotation_speed 
+		pitch = clamp(pitch - event.relative.y * rotation_speed, -PI/2.+.01, PI/2.+.01)
+		_update_shader_camera()
+	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			distance = max(distance * 0.9, 10.)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			distance = min(distance * 1.1, 1.0e3)
+		_update_shader_camera()
+
+func _update_shader_camera():
+	var x = distance * cos(pitch) * sin(yaw)
+	var y = distance * sin(pitch)
+	var z = distance * cos(pitch) * cos(yaw)
+	var v = Vector3(x, y, z)
+
+	var camera_position = target - v
+
+	shader_mat.set_shader_parameter("camera_position", camera_position)
+	shader_mat.set_shader_parameter("camera_target", target)
+	shader_mat.set_shader_parameter("camera_fov", fov)
