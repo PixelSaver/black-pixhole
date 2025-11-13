5114a7e004b5d1bf75d43d643f7abb2f21ced802
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-11-12 22:56:33 -0800

    Half attempt at smooth scrolling

--- scriptsnscenes/hole_3d_attempt2/3_drk_4_att_2.gd
+++ scriptsnscenes/hole_3d_attempt2/3_drk_4_att_2.gd
@@ -3,18 +3,24 @@ class_name RK4Attempt2
 
 @onready var shader_mat := $SubViewportContainer/SubViewport/ShaderRect.material as ShaderMaterial
 @export var pause : Control 
+@export var min_distance := 10.0
+@export var max_distance := 1000.0
+@export var zoom_speed := 0.1
 var target := Vector3.ZERO
 var distance := 60
+var target_distance := 60
 var yaw := 0.0
 var pitch := 0.0
 var rotation_speed := 0.01
 var fov := 60.0
+var cam_t : Tween
 
 func _ready():
 	Global.rk4_att2 = self
 	pause.process_mode = Node.PROCESS_MODE_PAUSABLE
 	yaw = .5
 	pitch = -.3
+	cam_t = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUINT)
 	_update_shader_camera()
 
 func _input(event):
@@ -25,10 +31,9 @@ func _input(event):
 	
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			distance = max(distance * 0.95, 10.)
+			target_distance = clamp(target_distance * 0.95 - 10., min_distance, max_distance)
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			distance = min(distance * 1.05, 1.0e3)
-		_update_shader_camera()
+			target_distance = clamp(target_distance * 1.05 + 10., min_distance, max_distance)
 	
 	if Input.is_action_just_pressed("pause"):
 		if pause.process_mode == Node.PROCESS_MODE_DISABLED:
@@ -37,6 +42,11 @@ func _input(event):
 		else:
 			print("paused")
 			pause.process_mode = Node.PROCESS_MODE_DISABLED
+
+func _process(delta):
+	# Smoothly interpolate distance toward target_distance
+	distance = clamp(lerp(distance, target_distance, log(zoom_speed*delta*distance)), min_distance, max_distance)
+	_update_shader_camera()
 
 func _update_shader_camera():
 	var x = distance * cos(pitch) * sin(yaw)
