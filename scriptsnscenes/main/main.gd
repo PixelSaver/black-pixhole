58bfe804b96fa829ef5e15a6f4752d4562b6ca4d
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-12-05 12:57:39 -0800

    Added space to pause, c to toggle controls (and hide the button), and r to reload the game

--- scriptsnscenes/main/main.gd
+++ scriptsnscenes/main/main.gd
@@ -8,3 +8,7 @@ func _ready() -> void:
 	while not Global.runtime_inspector:
 		await get_tree().process_frame
 	Global.runtime_inspector.inspect(bh_node)
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("restart"):
+		get_tree().reload_current_scene()
--- scriptsnscenes/main/ui.gd
+++ scriptsnscenes/main/ui.gd
@@ -10,6 +10,15 @@ func _ready():
 #func _process(_delta: float) -> void:
 	#fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
 
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("controls"):
+		if fps_label.visible:
+			_on_controls_toggle(true)
+			controls_toggle.hide()
+		else:
+			_on_controls_toggle(false)
+			controls_toggle.show()
+
 func _on_controls_toggle(toggled_on:bool):
 	if toggled_on:
 		Global.runtime_inspector.hide()
