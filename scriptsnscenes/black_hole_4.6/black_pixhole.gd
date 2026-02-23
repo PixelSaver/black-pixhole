f8df53cc60114e21f51d7d0af206cacfdb02ac2a
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2026-02-23 08:48:24 -0800

    Added exports, sort of fps handling, and fixed window scale issue

--- scriptsnscenes/black_hole_4.6/black_pixhole.gd
+++ scriptsnscenes/black_hole_4.6/black_pixhole.gd
@@ -1,2 +1,27 @@
 extends Control
 class_name BlackPixhole
+
+@export var vpcont : SubViewportContainer
+@export var vp : SubViewport
+@export var rect : ColorRect
+@export var target_fps : float = 30
+var _accum := 0.0
+var _time := 0.0
+var shader_mat : ShaderMaterial
+
+func _ready():
+	vp.render_target_update_mode = SubViewport.UPDATE_DISABLED
+	shader_mat = rect.material
+
+func _process(delta):
+	# FPS Calculations
+	_accum += delta
+	
+	var interval := 1.0 / target_fps
+	if _accum >= interval:
+		_accum -= interval
+		vp.render_target_update_mode = SubViewport.UPDATE_ONCE
+	
+	# Shader time setup
+	_time += delta
+	shader_mat.set_shader_parameter("time", _time)
